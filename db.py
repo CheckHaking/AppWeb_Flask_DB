0cb783474e85d624aef53c4368f36868dc85bae0
CheckHaking <sergiomantunez@gmail.com>
2024-03-19 19:34:41 -0600

    update

---
+++ db.py
@@ -0,0 +1,22 @@
+#FICHERO DE CONFIGURACION
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#Seguridad, acceso, deberia ir en este file
+#ENGINE permite a SQLAlchemy comunicarse con la base de datos en un dialecto concreto
+# docs https://docs.sqlalchemy.org/en/13/dialects/index.html
+#engine = create_engine('sqlite:///Carpeta_donde_se_va_guardar(database)/personas.db')
+engine = create_engine('sqlite:///database/tareas.db',
+                       connect_args={'check_same_thread': False})
+
+
+#ADVERTENVIA: Crear el engine no conecta inmediatamente con l DB, eso lo hacemos luego
+
+#Creamos La sesion es el objeto que nos va ayuadar en las transacciones dentro de nuestra DB
+Session = sessionmaker(bind=engine)
+session = Session() #este es nuestra conexion con la base de datos para modificar la base de datos
+
+#Ahora vamos al archivo models.py en los modelos(clases) donde queremos que se transformen en tablas
+#le aniadiremos esta variable y esto se encraga de mapear y vincular cada clase a cada tabla
+Base = declarative_base() #es la encargada de decirle que nuestra clases se convertiran entablas
+
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #Levantaremos el servider web con Flask
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #Portada de la pagina
+    #Retornar un fichero HTML
+    return render_template('index.html')
+@app.route('/crear-tarea')
+def crearTarea():
+    return 'Crear Tarea'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
